f29aaf2c01e0fcd17650ac8312efd4a30c927148
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-05 20:59:15 +0530

    Done with

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import ReactDOM from 'react-dom/client'
 import Home from "./Home"
 import About from './About';
 import Contact from './Contact';
-
+const current_path = window.location.pathname
 const root = ReactDOM .createRoot(document.getElementById('root'));
-root.render(<h1>Home page</h1>);
+
+if (current_path==="/") {
+  root.render(<Home/>)
+}
+else if (current_path==="/About")
+{
+  root.render(<About/>)
+}
+else if (current_path==="/Contact")
+  {
+    root.render(<Contact/>)
+  }
