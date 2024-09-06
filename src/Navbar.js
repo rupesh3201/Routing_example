bef16132ba3a165aa3318c4b394ddb42bddf0dbc
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-06 22:46:02 +0530

    a tag

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,6 +6,7 @@ function Navbar()
             <h1 className="navbar"> Navbar</h1>
             Kuch Mithha Ho Jaye  
             <div className="navbarlinks"> </div>
+            <a href="/"> Home </a>
         </div>
     )
 }
