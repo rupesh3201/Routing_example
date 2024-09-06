dc78ec1a9c0035e0ebc6b8cc56763f08a7515bcf
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-06 22:44:15 +0530

    Navbar links

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,6 +4,8 @@ function Navbar()
     return (
         <div>
             <h1 className="navbar"> Navbar</h1>
+            Kuch Mithha Ho Jaye  
+            <div className="navbarlinks"> </div>
         </div>
     )
 }
