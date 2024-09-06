88683fb4704341ccddd34258d74583f75242facf
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-06 23:01:52 +0530

    addeding css

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,17 +2,17 @@ import React from "react";
 function Navbar()
 {
     return (
-        <div>
-            <h1 className="navbar"> Navbar</h1>
+        <div className="navnbar_con">
+            <h1 className=""> Navbar</h1>
             Kuch Mithha Ho Jaye  
-            <div className="navbarlinks"> </div>
+            <div className="navbarlinks_con"> 
             <a href="/"> Home </a>
             {/* added home */}
             <a href="/About"> About </a>
             {/* added about */}
             <a href="/Contact"> Contact </a>
             {/* added  Contact */}
-
+            </div>
         </div>
     )
 }
